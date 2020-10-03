48c647f78e97e531524e91bbffd3d28a2d3964dc
TylerBoyd <tboyd1898@gmail.com>
2020-10-02 19:22:49 -0500

    added useful comments

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -11,19 +11,26 @@ class ViewController: UIViewController {
 
     @IBAction func enableDarkMode(_ sender: Any) {
         
+        // conditional to decide current theme
         if view.backgroundColor == UIColor.black {
             view.backgroundColor = UIColor.white
             
+            // place all subviews into an array
             let viewSubviews = view.subviews
+            // loop through subviews
             for eachView in viewSubviews {
+                // for each subview, decide if its a label
                 if eachView is UILabel {
+                    // if it is a label, downcast it specifically as a label
                     let currentLable = eachView as! UILabel
+                    // take that variable, depicting the currently selected subview, and make the text black
                     currentLable.textColor = UIColor.black
                 }
             }
         } else {
             view.backgroundColor = UIColor.black
             
+            // same as above, but if the theme was in dark mode
             let viewSubviews = view.subviews
             for eachView in viewSubviews {
                 if eachView is UILabel {
